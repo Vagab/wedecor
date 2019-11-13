6ebbc93c3e57e667d075d68971925e7cea43c0fc
Vagab <bilyksergeaa@gmail.com>
2019-11-13 18:59:51 +0100

    a bit refac

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,10 +15,8 @@ class ProductsController < ApplicationController
   end
 
   def load_products
-    # @products = Product.all
     @search = Product.ransack(params[:q])
     @products = @search.result
-    # @search.build_condition
   end
 
 end
